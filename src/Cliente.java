fad88febb435af07730b77df4579bafe02817e15
Lukas Rodrigo <lukasrar001@gmail.com>
2019-09-13 22:31:53 -0300

    inicio do projeto com mockito

---
+++ src/Cliente.java
@@ -0,0 +1,9 @@
+
+public class Cliente {
+
+	public double getPercentualDesconto() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/Pedido.java
@@ -0,0 +1,9 @@
+
+public class Pedido {
+
+	public double getValorPedido() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
